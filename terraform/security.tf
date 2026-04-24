c7e97cd661e66832bb094e740dc5cc735e3f5a3c
kondalmoganti <mkondalrao@gmail.com>
2026-04-24 12:53:09 +0200

    DevOps project: AWS + Terraform + Ansible + Docker voting app

--- terraform/security.tf
+++ terraform/security.tf
@@ -86,14 +86,21 @@ resource "aws_security_group" "db_sg" {
   description = "Allow PostgreSQL from backend instance"
   vpc_id      = aws_vpc.main.id
 
-ingress {
-  description     = "SSH from frontend SG"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  security_groups = [aws_security_group.frontend_sg.id]
-}
+  ingress {
+    description     = "SSH from frontend SG"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
 
+  ingress {
+    description     = "Postgres from frontend SG for result app"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
 
   ingress {
     description     = "Postgres from backend SG"
